892755d3ec8403be49338b5a93674e0524514aff
Robert Carbajal <robert@roberts-mbp.int.wellmark.com>
2015-12-10 19:09:06 -0600

    Fixed logic error with birth/death year data generation that was causing 1950 to always be the year with the most people alive, added data processing.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 var MIN_YEAR = 1900;
 var MAX_YEAR = 2000;
 
+var generateBtn, dataDisplay, numRecordsField, processBtn, resultsDisplay;
+var data = new Array();
+var results = new Array();
+
 $(document).ready(function() {
-	$("#generate").click(function() {
-		var dataDisplay = document.getElementById("data_display");
-		var recValue = document.getElementById("num_records").value.trim();
+	generateBtn = document.getElementById("generate");
+	dataDisplay = document.getElementById("data_display");
+	numRecordsField = document.getElementById("num_records");
+	processBtn = document.getElementById("process");
+	resultsDisplay = document.getElementById("results_display");
+
+	$(generateBtn).click(function() {
+		var recValue = numRecordsField.value.trim();
 		
-		if(recValue != "" && !isNaN(recValue) && recValue.indexOf(".") == -1) { //make sure we're getting only integers in our data field
+		if(recValue != "" && !isNaN(recValue) && recValue.indexOf(".") == -1 && recValue > 0) { //make sure we're getting only positive integers in our data field
 			dataDisplay.innerHTML = "";
 			
-			var data = generateData(recValue);
+			data = generateData(recValue);
 			var str = "";
 			for(var i = 0; i < data.length; ++i) {
 				str = data[i].lastName + "," + data[i].firstName + "," + data[i].birthYear + "," + data[i].deathYear;
 				dataDisplay.innerHTML += str + "<br />";
 			} //end for
 		} //end if
-		else alert("Please enter an integer value.");
+		else alert("Please enter a positive integer value.");
 	}); //end click
+
+	$(processBtn).click(function() {
+		if(data && data.length > 0) {
+			results = processData();
+
+			resultsDisplay.innerHTML = "Maximum number of people alive: " + results.max + "<br />" + "Years with max people alive:<br />";
+			for(var i = 0; i < results.years.length; ++i)
+				resultsDisplay.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;" + results.years[i] + "<br />";
+		} //end if
+		else alert("Please generate a dataset before attempting to process.");
+	}); //end click
+
+	$(generateBtn).click(); //generate an initial set of data
 }); //end ready
 
 function generateData(numToGenerate) {
@@ -25,14 +47,13 @@ function generateData(numToGenerate) {
 
 	var fnLength = FIRST_NAMES.length; //first and last names arrays are defined in names.js
 	var lnLength = LAST_NAMES.length;
-	var middleYear = Math.round((MAX_YEAR + MIN_YEAR) / 2); //birth years will be the first half of the year range and death years will be the latter half of the year range just to make things easy
 	for(var i = 0; i < numToGenerate; ++i) {
 		//create randomly generated person
 		var p = new Person();
 		p.firstName = FIRST_NAMES[getRandomNumber(0, fnLength - 1)];
 		p.lastName = LAST_NAMES[getRandomNumber(0, lnLength - 1)];
-		p.birthYear = getRandomNumber(MIN_YEAR, middleYear);
-		p.deathYear = getRandomNumber(middleYear + 1, MAX_YEAR);
+		p.birthYear = getRandomNumber(MIN_YEAR, MAX_YEAR);
+		p.deathYear = getRandomNumber(p.birthYear, MAX_YEAR);
 		
 		//add to our list
 		people.push(p);
@@ -40,6 +61,36 @@ function generateData(numToGenerate) {
 	
 	return people;
 } //end function generateData
+
+function processData() {
+	var resultsArr = new Array();
+	var years = new Object();
+
+	//count up how many times each year appears in the data set
+	var person = null;
+	for(var i = 0; i < data.length; ++i) {
+		person = data[i];
+		for(var j = person.birthYear; j <= person.deathYear; ++j)
+			if(years[j] == null)
+				years[j] = 1;
+			else years[j] += 1;
+	} //end foreach
+
+	//find the years with the largest counts
+	var maxCount = 0;
+	var yearsArr = new Array();
+	for(var year in years) {
+		if(years[year] > maxCount) {
+			maxCount = years[year]; //take note of the current max count
+			yearsArr = new Array(); //start a new list of years with max count of people alive
+			yearsArr.push(year);
+		} //end if
+		else if(years[year] == maxCount)
+			yearsArr.push(year); //add to list of years with max count of people alive
+	} //end foreach
+
+	return { max: maxCount, years: yearsArr };
+} //end function processData
 
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
